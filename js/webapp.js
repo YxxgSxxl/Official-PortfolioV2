1d4cc3f461540455ef750eeab61bcb1ae5a7f6c6
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-02-14 11:12:27 +0100

    WebApp init

--- js/webapp.js
+++ js/webapp.js
@@ -1,4 +1,10 @@
-import express from 'express';
+const express = require('express');
 
+// Create the server
 const app = express();
 
+// Launch the server
+app.listen(5000);
+console.log(app);
+console.info("Server started on http://localhost:5000/");
+console.info("app");
