48a5bb090b34423f61ffdc39c8656097404c6cdf
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-02-14 20:07:16 +0100

    up

--- index.php
+++ index.php
@@ -27,27 +27,12 @@
 
     <!-- Main Content -->
     <main>
-        <h1>In devlopment...</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora ipsum dolores ratione quidem vel libero nostrum debitis expedita id dolorum numquam ipsa at, explicabo deserunt, officia iusto. Ullam, libero quae.
-        Atque ad consectetur fugit. Nemo, vitae. Odio eveniet, est assumenda dolorem quos, voluptatum at ipsum sed ipsa nesciunt facilis sit ipsam iusto, error officiis nisi consequuntur nam inventore delectus unde?
-        Aut sunt eum delectus iste, reprehenderit labore optio ratione repellat cumque, accusamus possimus facilis velit ab, culpa consequatur exercitationem numquam quia quam fuga voluptatibus. Voluptatem eveniet aspernatur debitis eaque itaque.
-        Quisquam, eius deleniti. Nulla at architecto, tempore possimus itaque non hic, fugiat error labore consectetur tenetur sit ducimus autem voluptatem, natus alias recusandae accusamus aliquid enim odit harum repellendus aspernatur?
-        Adipisci laudantium totam quaerat dolor eligendi deleniti velit eum est obcaecati ad, incidunt quas, natus et doloribus suscipit in facilis, commodi voluptates eos tenetur explicabo similique debitis? Animi, cumque non.
-        A magnam, quidem nam obcaecati distinctio ducimus delectus quod facilis expedita. Delectus cum iusto vero, asperiores suscipit magnam? Saepe repellendus totam ratione porro nesciunt quia ipsam illum, neque nemo minus.
-        Rerum aperiam quasi non maxime dolor voluptatem a magni unde, libero, iure nostrum dolorum, illo enim eligendi sed nesciunt ullam quos. Numquam sint corrupti possimus repudiandae qui illo ducimus id.
-        Culpa esse soluta ullam tempore impedit rem numquam, sapiente blanditiis, sequi architecto molestiae quisquam, enim atque. Dolorem blanditiis sunt labore ab, quod totam inventore quisquam nemo reprehenderit in repellendus id.
-        Fugit autem hic asperiores nam iure placeat tenetur similique explicabo cupiditate? Aliquid eligendi, ducimus rem libero culpa eveniet. Non provident vero quasi quia aut suscipit laudantium sequi excepturi odio placeat!
-        Ad a nam eaque laborum et, labore ducimus molestias nostrum, magnam voluptas non excepturi ipsum assumenda enim sit repellat illum nisi repellendus officiis iusto asperiores incidunt velit similique suscipit. Debitis?
-        Repudiandae, quidem eos voluptatem aliquid rem similique, fugit inventore deserunt voluptatibus nam eveniet voluptate corporis aspernatur nostrum quo. Optio, laudantium ut rem velit ipsam fugit. Consectetur porro optio quae ut.
-        Error porro magnam a molestiae sunt, nulla deserunt debitis maiores placeat voluptas fuga quasi! Praesentium est odit repellat? Alias autem est voluptate culpa saepe nulla tenetur in architecto aliquid repudiandae!
-        Laboriosam illum ipsum blanditiis eveniet enim. Asperiores voluptas reiciendis placeat ex! Suscipit perferendis in dolores totam molestias! Placeat iusto, dignissimos, saepe ratione sequi error, illo voluptate eveniet repudiandae reprehenderit alias!
-        Saepe natus explicabo distinctio. Beatae illum ea molestias quas magni voluptatum recusandae repellendus consequatur. Ducimus ipsa itaque nobis magni, at impedit ratione dolore, dicta voluptates placeat voluptatibus fugit possimus esse.
-        Voluptatem, id veritatis eius, nam enim, nostrum earum laudantium asperiores fuga exercitationem rerum modi. Totam doloremque illo recusandae fuga perferendis ipsum aliquam, quam, accusantium, a assumenda quo sequi? Commodi, beatae!
-        Sint vel illo tenetur est voluptatum in, facilis dolores similique iusto. Non, quae. Voluptatibus mollitia commodi odit quaerat laboriosam consequuntur? Molestiae tempora officia quisquam voluptas impedit quibusdam? Officiis, delectus fugit.
-        Cumque, repudiandae culpa adipisci deleniti dolorem molestias officia earum. Quia aliquam reprehenderit quam quos inventore natus, dicta reiciendis aspernatur, illo laudantium similique pariatur amet eum perferendis nisi expedita minus nulla?
-        Suscipit temporibus, aperiam sed aspernatur sequi ab facilis, dolore corrupti consequatur impedit amet non in incidunt vel nihil eveniet neque perspiciatis sunt magnam modi. Asperiores ullam deleniti nam libero deserunt.
-        Saepe tempore error ducimus laborum nostrum, fugit autem numquam cum sequi porro et perspiciatis pariatur deleniti inventore dolorum temporibus! Nemo neque illum porro hic eum autem similique dolor deleniti distinctio.
-        Eligendi soluta reiciendis autem eveniet laboriosam nostrum laudantium! Voluptates dolor provident porro ipsam, asperiores vero sapiente error quas aliquam fugiat excepturi, expedita dolore dolores unde odio deserunt, vitae doloremque cupiditate.</p>
+        <section class="home-container">
+            <div class="home-content">
+                <h1>Hi, I'm Alex Eckes</h1>
+                <p>Web Developer</p>
+            </div>
+        </section>
     </main>
 
     <!-- Including the Footer Component -->
