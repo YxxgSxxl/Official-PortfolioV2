4b47722fe219ef620766c08282219fe8e1f20206
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-02-14 11:58:13 +0100

    Remove Express

--- js/server.js
+++
@@ -1,9 +0,0 @@
-// import '/dotenv/config';
-import express from 'express';
-
-// Create the server
-const app = express();
-
-// Launch the server
-app.listen(process.env.PORT);
-console.info("Server started on http://localhost:5000/");
