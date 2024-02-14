0bfb6fc200ac4c5bf1c463f3a92826e55cacba0d
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-02-14 19:54:18 +0100

    js for header added + responsive

---
+++ js/index.js
@@ -0,0 +1,27 @@
+////////////////////////// HEADER COMPONENT //////////////////////////
+// burger Menu //
+let burgerIcon = document.querySelector(".header-burgericon"); // burgerIcon to open headerMenu
+let exitMenu = document.querySelector(".header-menuexit") // Cross to close headerMenu
+let headerMenu = document.querySelector(".header-menu"); // headerMenu with all the hyperlinks
+let menuTag = document.querySelectorAll("ul li a"); // All headerMenu hyperlinks
+
+
+// Burger Menu Opening
+burgerIcon.addEventListener("click", () => {
+    headerMenu.classList.toggle('header-open')
+    burgerIcon.classList.add('no-visibility')
+})
+
+// Closing Header Menu
+exitMenu.addEventListener("click", () => {
+    headerMenu.classList.toggle('header-open')
+    burgerIcon.classList.remove('no-visibility')
+})
+
+// Closing Header Menu When Clicking On A Hyperlink
+for (let i = 0; i < menuTag.length; i++) {
+    menuTag[i].addEventListener("click", function() {
+        headerMenu.classList.toggle("header-open");
+        burgerIcon.classList.remove('no-visibility')
+      });
+}
