9276be6e18b1a17d03abc9c541b28f503eb39fa5
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-02-14 22:30:53 +0100

    responsive, gabarit component and html added

--- components/footer.php
+++ components/footer.php
@@ -8,24 +8,8 @@
         <a href="#">Sitemap</a>
     </div>
     <div class="footer-right">
-        <a title="Github" target="_blank" href="https://github.com/YxxgSxxl">
-            <svg width="60%" height="60%" viewBox="0 0 24 24" stroke="#ffffff" fill="none"
-                xmlns="http://www.w3.org/2000/svg" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
-                stroke-linejoin="round">
-                <path
-                    d="M16 22.027v-2.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7a5.44 5.44 0 00-1.5-3.75 5.07 5.07 0 00-.09-3.77s-1.18-.35-3.91 1.48a13.38 13.38 0 00-7 0c-2.73-1.83-3.91-1.48-3.91-1.48A5.07 5.07 0 005 5.797a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7a3.37 3.37 0 00-.94 2.58v2.87M9 20.027c-3 .973-5.5 0-7-3">
-                </path>
-            </svg>
-        </a>
-        <a title="LinkedIn" target="_blank" href="https://www.linkedin.com/in/alex-eckes/">
-            <svg width="60%" height="60%" viewBox="0 0 24 24" stroke="#ffffff" fill="none"
-                xmlns="http://www.w3.org/2000/svg" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
-                stroke-linejoin="round">
-                <path d="M21 8v8a5 5 0 01-5 5H8a5 5 0 01-5-5V8a5 5 0 015-5h8a5 5 0 015 5zM7 17v-7">
-                </path>
-                <path d="M11 17v-3.25M11 10v3.75m0 0c0-3.75 6-3.75 6 0V17M7 7.01l.01-.011"></path>
-            </svg>
-        </a>
+        <a href="https://github.com"><img src="./img/icons/github.svg" alt="GitHub"></a>
+        <a href="https://linkedin.com"><img src="./img/icons/linkedin.svg" alt="LinkedIn"></a>
     </div>
-
+    
 </footer>
