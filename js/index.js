e1ffa9435eddc75828431a1343fd2a72e24fd5bd
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-02-14 22:04:50 +0100

    programming...

--- js/index.js
+++ js/index.js
@@ -8,20 +8,20 @@ let menuTag = document.querySelectorAll("ul li a"); // All headerMenu hyperlinks
 
 // Burger Menu Opening
 burgerIcon.addEventListener("click", () => {
-    headerMenu.classList.toggle('header-open')
-    burgerIcon.classList.add('no-visibility')
+    headerMenu.classList.toggle('header-open');
+    burgerIcon.classList.add('no-visibility');
 })
 
 // Closing Header Menu
 exitMenu.addEventListener("click", () => {
-    headerMenu.classList.toggle('header-open')
-    burgerIcon.classList.remove('no-visibility')
+    headerMenu.classList.toggle('header-open');
+    burgerIcon.classList.remove('no-visibility');
 })
 
 // Closing Header Menu When Clicking On A Hyperlink
 for (let i = 0; i < menuTag.length; i++) {
     menuTag[i].addEventListener("click", function() {
         headerMenu.classList.toggle("header-open");
-        burgerIcon.classList.remove('no-visibility')
+        burgerIcon.classList.remove('no-visibility');
       });
 }
