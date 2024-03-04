4db2083a1b0c499ee26ffa2372fd7c1e3ef4fe51
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-03-04 19:52:42 +0100

    reset

--- components/contactForm.php
+++ components/contactForm.php
@@ -2,7 +2,7 @@
 null
     ?>
 
-<form class="contact-form" method="post" action="">
+<form class="contact-form" method="post" action="sendMail.php">
     <input type="text" placeholder="Nom" name="name" id="contact-name" autocomplete="off" required>
     <input type="email" placeholder="Email" name="email" id="contact-email" autocomplete="off" required>
     <textarea name="message" id="contact-message" cols="30" rows="10" placeholder="Message" autocomplete="off" required></textarea>
