b8c2e4bbac441f90a4b48062379e58b871f8acd3
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-03-04 19:25:00 +0100

    mailer added

--- components/contactForm.php
+++ components/contactForm.php
@@ -3,8 +3,8 @@ null
     ?>
 
 <form class="contact-form" method="post" action="">
-    <input type="text" placeholder="Nom" name="name" id="contact-name" required>
-    <input type="email" placeholder="Email" name="email" id="contact-email" required>
-    <textarea name="message" id="contact-message" cols="30" rows="10" placeholder="Message" required></textarea>
-    <input type="submit" value="Envoyer" class="submit-button" id="submit-button">
+    <input type="text" placeholder="Nom" name="name" id="contact-name" autocomplete="off" required>
+    <input type="email" placeholder="Email" name="email" id="contact-email" autocomplete="off" required>
+    <textarea name="message" id="contact-message" cols="30" rows="10" placeholder="Message" autocomplete="off" required></textarea>
+    <input type="submit" value="Envoyer" class="submit-button" id="submit-button" autocomplete="off">
 </form>
