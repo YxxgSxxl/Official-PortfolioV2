7ed3c310563304c24155e4dd732242f3e5c52541
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-03-04 20:23:45 +0100

    reset

--- components/contactForm.php
+++ components/contactForm.php
@@ -2,9 +2,10 @@
 null
     ?>
 
-<form class="contact-form" method="post" action="sendMail.php">
-    <input type="text" placeholder="Nom" name="name" id="contact-name" autocomplete="off" required>
-    <input type="email" placeholder="Email" name="email" id="contact-email" autocomplete="off" required>
-    <textarea name="message" id="contact-message" cols="30" rows="10" placeholder="Message" autocomplete="off" required></textarea>
+<form class="contact-form" method="post" action="./components/sendMail.php">
+    <input type="text" placeholder="Nom *" name="name" id="contact-name" autocomplete="off" required>
+    <input type="email" placeholder="Email *" name="email" id="contact-email" autocomplete="off" required>
+    <input type="text" placeholder="Sujet *" name="subject" id="contact-subject" autocomplete="off" required>
+    <textarea name="message" id="contact-message" cols="30" rows="10" placeholder="Message *" autocomplete="off" required></textarea>
     <input type="submit" value="Envoyer" class="submit-button" id="submit-button" autocomplete="off">
 </form>
--- components/sendMail.php
+++ components/sendMail.php
@@ -1,2 +1,19 @@
 <?php
-var_dump($_POST);
+// Make every POST response a variable
+foreach ($_POST as $key => $value) {
+    ${$key} = $value;
+    // echo $name;
+    // echo $email;
+    // echo $subject;
+    // echo $message;
+}
+
+$mailHeader = "From:" . $name . "<" . $email . ">\r\n";
+
+$recipient = "contact.eckesalex14@gmail.com";
+
+mail($recipient, $subject, $message, $mailHeader)
+or die("Error!");
+
+
+header('Location: ../index.php');
