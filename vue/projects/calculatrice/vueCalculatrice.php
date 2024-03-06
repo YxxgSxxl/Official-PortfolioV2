8036d86c38085d7592bba0423ad2cbee254c4c9b
YxxgSxxl <114391274+YxxgSxxl@users.noreply.github.com>
2024-03-06 16:55:14 +0100

    Projects first content added

--- vue/projects/calculatrice/vueCalculatrice.php
+++ vue/projects/calculatrice/vueCalculatrice.php
@@ -10,7 +10,7 @@ null
             Pour ce projet, j'ai suivi un guide afin d'apprendre certaines notions de JS.
         </p>
         <p>
-            J'ai appris à utiliser des systèmes d'évenements récupérer des informations et intéragir avec.
+            J'ai appris à utiliser des systèmes d'évenements afin de récupérer des informations et intéragir avec.
         </p>
         <a href="vue/projects/calculatrice/app/calculatrice.html" target="_blank">
             <button id="submit-button">Voir le projet</button>
--- vue/projects/portfoliov2/vuePortfoliov2.php
+++ vue/projects/portfoliov2/vuePortfoliov2.php
@@ -7,7 +7,9 @@ null
     <section class="project-container">
         <h1>Portfolio V2</h1>
         <p>
-            Pour ce projet, j'ai d'abord du me procurer un nom de domain (DNS) ainsi qu'un serveur web. J'ai opté pour un VPS (Virtual Private Server) de petite capacité de chez <a href="https://www.ionos.fr/">IONOS</a>.
+            Pour ce projet, j'ai d'abord du me procurer un nom de domain (DNS) ainsi qu'un serveur web. J'ai opté pour
+            un VPS (Virtual Private Server) de petite capacité de chez <a href="https://www.ionos.fr/"
+                target="_blank" style="color: white; text-decoration: none; background-color: #3c4c54;">IONOS</a>.
         </p>
         <p>
             J'ai appris à utiliser des systèmes d'évenements récupérer des informations et intéragir avec.
@@ -15,6 +17,7 @@ null
         <a href="https://alexeckes.com" target="_blank">
             <button id="submit-button">Voir le projet</button>
         </a>
-        <small style="font-size: 0.7em; transform: translateY(-10px); text-align: start;">Vous êtes actuellement sur le projet ^^'</small>
+        <small style="font-size: 0.7em; transform: translateY(-10px); text-align: start;">Vous êtes actuellement sur le
+            projet ^^'</small>
     </section>
 </main>
